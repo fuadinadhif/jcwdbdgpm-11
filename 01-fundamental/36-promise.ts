7a31da30ac0eab61d40cc88a101a9f6c987a71b0
fuadinadhif <nadhiffuadi@gmail.com>
2026-02-11 16:38:50 +0700

    add new materials

---
+++ 01-fundamental/36-promise.ts
@@ -0,0 +1,60 @@
+{
+  /* ---------------------------------- Basic --------------------------------- */
+  // Promise statuses:
+  // 1. Pending
+  // 2. Resolve
+  // 3. Reject
+
+  const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (true) {
+        return resolve("Promise success");
+      } else {
+        return reject("Promise rejected");
+      }
+    }, 1000);
+  });
+
+  // 1. Dot then
+  console.log(myPromise);
+
+  // myPromise
+  //   .then((data) => console.log(data))
+  //   .catch((error) => console.error(error));
+
+  /* --------------------------------- Example -------------------------------- */
+  function boilWater() {
+    return new Promise((resolve) => {
+      const delay = Math.ceil(Math.random() * 2000);
+      setTimeout(() => {
+        console.log("Water boiled");
+        resolve("");
+      }, delay);
+    });
+  }
+
+  function addCoffee() {
+    return new Promise((resolve) => {
+      const delay = Math.ceil(Math.random() * 2000);
+      setTimeout(() => {
+        console.log("Coffee added");
+        resolve("");
+      }, delay);
+    });
+  }
+
+  function serveCoffee() {
+    return new Promise((resolve) => {
+      const delay = Math.ceil(Math.random() * 2000);
+      setTimeout(() => {
+        console.log("Coffee served ☕");
+        resolve("");
+      }, delay);
+    });
+  }
+
+  boilWater()
+    .then(() => addCoffee())
+    .then(() => serveCoffee())
+    .then(() => console.log("Enjoy!"));
+}
