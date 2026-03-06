0fd91f81878d05ec75ccaaabd77a2c44e85d8d6b
fuadinadhif <nadhiffuadi@gmail.com>
2026-03-06 15:05:16 +0700

    add new materials

--- 03-backend/01-blog/SETUP.md
+++ 03-backend/01-blog/SETUP.md
@@ -1,9 +1,31 @@
 # SETUP
 
+## Initial Setup
+
 1. `npm init --y`: Init file `package.json`
 2. Buat folder `src`
 3. Di dalam folder `src` buat file `app.ts` | `index.ts` | `server.ts`
 4. `npm install express`
-5. `npm install -D typescript tsx @types/node @types/express`
+5. `npm install -D typescript @types/node @types/express`
 6. `npx tsc --init`: Init file `tsconfig.json`
 7. Enable `rootDir`, `outDir`, `lib`, dan `types` di file `tsconfig.json`
+
+## Additional Package
+
+1. `npm i pg` & `npm i -D @types/pg`: Untuk menghubungkan server express dengan database
+
+## Prisma Setup
+
+1. `npx prisma init`
+   - `/prisma/schema.prisma`
+   - `/.env`
+   - `/prisma.config.ts`
+2. `npm i dotenv`
+3. `npm install @prisma/client @prisma/adapter-pg`
+4. `npm i -D prisma`
+5. Selesaikan error di file `prisma.config.ts` dan `tsconfig.ts`
+6. Ubah `DATABASE_URL` ke `DIRECT_URL` di `prisma.config.ts`
+7. Sesuaikan environment variable di file `.env` agar sama dengan `.env` Supabase project
+8. `npx prisma db pull`
+9. `npx prisma db push`
+10. `npx prisma generate`
--- 03-backend/01-blog/src/app.ts
+++ 03-backend/01-blog/src/app.ts
@@ -1,21 +1,122 @@
+import "dotenv/config";
+
 import express from "express";
-import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client.js";
 
 const app = express();
 
-// setup database connection
-const pool = new Pool({
-  user: "postgres.mrzuqzoaxvsfxqpemxio",
-  host: "aws-1-ap-northeast-1.pooler.supabase.com",
-  database: "postgres",
-  password: "purwadhika123",
-  port: 5432,
+/* -------------------------- Membuat prisma client ------------------------- */
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+const prisma = new PrismaClient({ adapter });
+
+/* ------------------------------- Middlewares ------------------------------ */
+app.use(express.json()); // middleware untuk mengambil data dari request client
+
+/* ---------------------------- Get all articles ---------------------------- */
+app.get("/api/articles", async (req, res) => {
+  const page = +req.query.page! || 1;
+  const limit = +req.query.limit! || 3;
+  const skip = (page - 1) * limit;
+
+  const articles = await prisma.article.findMany({
+    skip,
+    take: limit,
+    include: {
+      authors: { select: { name: true, email: true } },
+      articleTags: true,
+    },
+  });
+
+  const articleCount = await prisma.article.count();
+
+  res.status(200).json({
+    data: articles,
+    page,
+    totalArticles: articleCount,
+    totalPages: Math.ceil(articleCount / limit),
+  });
 });
 
-/* --------------------------------- GET ALL -------------------------------- */
-app.get("/api/articles", async (req, res) => {
-  const articles = await pool.query(`SELECT * FROM articles`);
-  res.status(200).json({ data: articles.rows });
+/* ----------------------------- Get all authors ---------------------------- */
+app.get("/api/authors", async (req, res) => {
+  const authors = await prisma.author.findMany({
+    omit: { password: true },
+    where: { deleted_at: null },
+  });
+  res.status(200).json({ data: authors });
+});
+
+/* ------------------------- Get single author by id ------------------------ */
+app.get("/api/authors/:id", async (req, res) => {
+  const id = +req.params.id;
+  const author = await prisma.author.findUnique({
+    where: { id, deleted_at: null },
+  });
+  res.status(200).json({ data: author });
+});
+
+/* ---------------------------- Create new author --------------------------- */
+app.post("/api/authors", async (req, res) => {
+  const userInput = req.body;
+
+  if (!userInput.name)
+    return res.status(400).json({ message: "Field name is missing" });
+  if (!userInput.email)
+    return res.status(400).json({ message: "Field email is missing" });
+  if (!userInput.password)
+    return res.status(400).json({ message: "Field password is missing" });
+
+  const author = await prisma.author.create({ data: userInput });
+  res.status(201).json({ message: "Author created", data: author });
+});
+
+/* --------------------------- Update author data --------------------------- */
+app.put("/api/authors/:id", async (req, res) => {
+  const userInput = req.body;
+  const id = +req.params.id;
+  const updatedAuthor = await prisma.author.update({
+    where: { id },
+    data: userInput,
+  });
+  res
+    .status(200)
+    .json({ message: `Author with id: ${id} updated`, data: updatedAuthor });
+});
+
+/* -------------------------- Delete single author -------------------------- */
+// HARD
+app.delete("/api/authors/:id", async (req, res) => {
+  const id = +req.params.id;
+  await prisma.author.delete({ where: { id } });
+  res.status(200).json({ message: `Author with id: ${id} deleted` });
+});
+
+// SOFT
+app.put("/api/authors/soft-delete/:id", async (req, res) => {
+  const id = +req.params.id;
+  await prisma.author.update({
+    where: { id },
+    data: { deleted_at: new Date() },
+  });
+  res.status(200).json({ message: `Author with id: ${id} (soft) deleted` });
+});
+
+/* --------------------------- Delete all authors --------------------------- */
+// HARD
+app.delete("/api/authors", async (req, res) => {
+  await prisma.author.deleteMany();
+  res.status(200).json({ message: "All authors deleted" });
+});
+
+// SOFT
+app.put("/api/authors/soft-delete", async (req, res) => {
+  await prisma.author.updateMany({
+    data: { deleted_at: new Date() },
+  });
+  res.status(200).json({ message: `All authors (soft) deleted` });
 });
 
 const PORT = 8000;
